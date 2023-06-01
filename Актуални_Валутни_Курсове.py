e65b8c7bf715eaf6abf14367569c30b7f5db113e
StanM84 <135264995+StanM84@users.noreply.github.com>
2023-06-01 22:59:07 +0300

    Add files via upload

---
+++ Актуални_Валутни_Курсове.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome()
+driver.get("https://united-km.com")
+
+try:
+    # Изчакваме до 10 секунди, докато елементът стане видим
+    element = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.XPATH, "//*[contains(text(), 'Актуални Валутни Курсове')]"))
+    )
+    print("Елементът 'Актуални Валутни Курсове' съществува на страницата.")
+except:
+    print("Елементът 'Актуални Валутни Курсове' не съществува на страницата.")
+
+driver.quit()
